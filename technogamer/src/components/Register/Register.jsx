3751590d0098f95ed4b9399df2aa3b2e5e1a3e78
FrancoGerez <franconicolasgerez1998@gmail.com>
2025-06-29 22:49:51 -0300

    fusion de front y back

--- technogamer/src/components/Register/Register.jsx
+++ technogamer/src/components/Register/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from '../button/Button'; // Asegúrate que existe
+import Button from '../button/Button';
 import './Register.css';
 
 function Register({ abrirModalLogin }) {
