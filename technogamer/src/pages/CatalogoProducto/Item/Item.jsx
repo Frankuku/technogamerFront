239ce0d7888837af1a70b42a56e445bea48b9726
Frankuku <franconicolasgerez1998@gmail.com>
2025-07-07 01:40:47 -0300

    Update Item.jsx

--- technogamer/src/pages/CatalogoProducto/Item/Item.jsx
+++ technogamer/src/pages/CatalogoProducto/Item/Item.jsx
@@ -1,24 +1,30 @@
-
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
-import { useCart } from "../../../hook/useCart.js"
+import { useCart } from "../../../hook/useCart.js";
 import './Item.css';
-import carrito from '../../../assets/img/iconos/carrito.png'
+import carrito from '../../../assets/img/iconos/carrito.png';
+
 function Item({ producto }) {
-  const { addToCart, cart } = useCart()
-  const checkProductInCart = product => {
-    return cart.some(item => item.id === product._id)
-  }
+  const { addToCart, cart } = useCart();
+  const navigate = useNavigate();
+
+  // Verifica si ya está en el carrito
+  const enCarrito = cart.some(item => item._id === producto._id);
+
+  const handleAddToCart = (e) => {
+    e.preventDefault(); // evita que se active el <Link>
+    e.stopPropagation();
+    addToCart(producto);
+    navigate("/carrito"); // redirige al carrito
+  };
 
   return (
-
     <Link to={`/producto/${producto._id}`} className="col-sm-12 col-md-5 col-lg-3 p-2">
-
-      <div className="card " >
+      <div className="card">
         <img
-          src={`https://technogamer.onrender.com${producto.image}`}
+          src={`http://localhost:4000${producto.image}`}
           className="card-img-top"
-          alt={producto.image}
+          alt={producto.name}
         />
         <div className="card-body d-flex flex-column justify-content-between">
           <div>
@@ -28,30 +34,31 @@ function Item({ producto }) {
                 : producto.name}
             </h5>
           </div>
+
           <div className="d-flex justify-content-between gap-2 mt-3 w-100 align-items-end">
             <p className="card-text">${producto.price}</p>
 
             {producto.stock > 0 ? (
-              <Button
-                className="btn-carrito"
-                onClick={(e) => {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  addToCart(producto);
-                  console.log("AGREGADO:", producto);
-                }}
-              >
-                <img src={carrito} alt="carrito" />
-              </Button>
+              enCarrito ? (
+                <Button variant="success" disabled>
+                  ✔ Agregado
+                </Button>
+              ) : (
+                <Button
+                  className="btn-carrito"
+                  onClick={handleAddToCart}
+                >
+                  <img src={carrito} alt="carrito" />
+                </Button>
+              )
             ) : (
               <span className="sin-stock">Sin stock</span>
             )}
           </div>
-
         </div>
       </div>
     </Link>
-  )
+  );
 }
 
-export default Item
+export default Item;
